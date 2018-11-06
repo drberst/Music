d282687d1fdf4b245022127151f081c6c16397ec
berst <berst@DESKTOP-PTPF73A.lan>
2018-11-06 16:27:15 -0500

    initial code to read tablature from file

--- music/src/main/java/music/MusicXML.java
+++ music/src/main/java/music/MusicXML.java
@@ -2,57 +2,103 @@ package music;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
-//import java.util.Arrays;
-//import java.util.Iterator;
-//import java.util.LinkedHashMap;
-//import java.util.LinkedList;
-//import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Collections;
 
-import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
-
 public class MusicXML {
 
 	public static void main(String[] args) throws Exception {
 		String filename = "foo.xml";
-//		filename = "rival.xml";
-		filename = "GameOverSimple.xml";
+//				filename = "rival.xml";
+//		filename = "GameOverSimple.xml";
 		Document document = parse(filename);
-		
+
 		toNoteList(document);
 	}
-	
-	private static void toNoteList(Document document) {
 
+	public static void main2(String[] args) throws Exception {
+		String filename = "zelda.tab";
+		List<String> linelist = readFileInList(filename);
+		String[] lines = linelist.toArray(new String[6]);
+//		int i =5;
+		int offset = 4;
+		int len = lines[0].length();
+		int dashesperquarter = 2;
+		int r=0;
+		int smallestR = len;
+		
+		Measure.width = 8;
+		int beat = 0;
+		Measure m = new Measure();
+		int mNumber = 1;
+
+		for (int pos = offset; pos < lines[0].length(); pos++) {
+//			Note = Guitar.strs[]
+			Noteg gg = null;
+
+			for (int str = 0; str < lines.length; str++) {
+				int interval = Character.getNumericValue(lines[str].charAt(pos));
+				if (interval >= 0) {
+					gg = Guitar.noteOnString(str, interval);
+					gg.duration = r;
+					m.put(gg, 0, beat);
+					beat+=r;
+					if(beat > Measure.beatsperbar) {
+						System.out.println("Measure: "+mNumber+++"\n"+m.toString());
+						m=new Measure();
+						beat = 0;
+					}
+					if (r> 0 && r < smallestR) smallestR = r;
+					r=0;
+				}
+				System.out.print(lines[str].charAt(pos));
+			}
+			if (gg==null) System.out.println(r++);
+			else System.out.println(" Note="+gg+"//"+gg.duration+"."+gg.subdiv);
+		}
+		System.out.println("Smallest r is "+smallestR);
+		m.trim();
+		System.out.println(m);
+	}
+	
+	
+	public static List<String> readFileInList(String filename) {
+		List<String> lines = Collections.emptyList();
+		try {
+			lines = Files.readAllLines(Paths.get(filename), StandardCharsets.UTF_8);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return lines;
+	}
+
+	private static void toNoteList(Document document) {
 		Element part = document.getRootElement().element("part");
 		int mNumber = 1;
 		for (Iterator<Element> itMeas = part.elementIterator("measure"); itMeas.hasNext();) {
-			
 			Element measure = itMeas.next();
-			
-//			if (mNumber == 3 || mNumber == 1) {
-				Measure m = Measure.parse(measure);
-				//			System.out.printf("------ Measure %s ------\n", m.number);
-				//			LinkedHashMap<Integer, Cluster> bar = new LinkedHashMap<Integer, Cluster>();
-				System.out.println(mNumber+" / / / / / / / / / / / /");
-				System.out.println(m);
-//			} 
+			Measure m = Measure.parse(measure);
+//			System.out.println(mNumber + " / / / / / / / / / / / /");
+			System.out.println(m.measureNumber + " / / / / / / / / / / / /");
+
+			System.out.println(m);
 			mNumber++;
 		}
-		//		return list;
 	}
-	public static void guitarNote() {
-		Note n = new Note();
-		n.alter = 0;
-		n.name = "G";
-		n.octave = 5;
-		
-	}
+
 	public static void edit(Document document) throws IOException {
 
 		Element root = document.getRootElement();
