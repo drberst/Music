136cb5cc47147232701ce21dc008f59c31038bf2
berst <berst@DESKTOP-PTPF73A.lan>
2018-11-06 16:26:48 -0500

    rework to better support uses outside of parsing musicxml

--- music/src/main/java/music/Measure.java
+++ music/src/main/java/music/Measure.java
@@ -1,6 +1,6 @@
 package music;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -9,29 +9,49 @@ import org.dom4j.Element;
 
 class Measure {
 	//	Chord[] groups;
-	static int width = 5;
 	Note[][] Notes;
-	int time;
-	int part;
+	int curtime;
+	int curpart;
+	int parts;
 	List<Attribute> attributes;
-	int number;
-	static int subdiv;
-	static int notesperbar;
-
+	int measureNumber;
+	
+	static int width = 6;
+	static int beatsperbar = 4;
+	static int notesperbeat = 4;
+	
+	public Measure() {
+//		quartervalue = 4;
+//		notesperbar = quartervalue * 4;
+		parts = 1;
+		Notes = new Note[parts][beatsperbar()];
+//
+		measureNumber = 1;
+		curtime = 0;
+		curpart = 0;
+	}
+	public int beatsperbar() {
+		return notesperbeat*beatsperbar;
+	}
 	static Measure parse(Element e) {
 		Measure m = new Measure();
-		if (subdiv == 0)
-			subdiv = Integer.parseInt(e.element("attributes").elementText("divisions"));
-		notesperbar = subdiv*4;
+		m.measureNumber = Integer.parseInt(e.attributeValue("number"));
+
+		if (m.measureNumber == 1) {
+			System.err.println("FIRST!!");
+			notesperbeat = Integer.parseInt(e.element("attributes").elementText("divisions"));
+		}
+		
+//		timesigTop = timesigBot * 4;
+//		System.err.println("subdiv is:"+quartervalue);
 		//		m.groups = new Chord[subdiv * 4];
 		//		m.parts = new ArrayList<>[subdiv*4];
 
-		m.number = Integer.parseInt(e.attributeValue("number"));
-		m.time = 0;
-		m.part = 0;
-		m.Notes = new Note[3][16];
+		m.curtime = 0;
+		m.curpart = 0;
+		m.Notes = new Note[5][m.beatsperbar()];
 		m.parseNotes(e);
-		m.trim();
+//		m.trim();
 		return m;
 	}
 
@@ -42,20 +62,30 @@ class Measure {
 			//			note.getName()
 			//			System.out.println(type);
 			if (type.equals("backup")) {
-				this.time = 0;
-				this.part++;
+				this.curtime = 0;
+				this.curpart++;
 			} else if (type.equals("note")) {
 				Note n = Note.parseElement(note);
-				if(n.toString().length() > width) width = n.toString().length();
-				if (n.chord)
-					time = 0;
-				put(n, part, time);
-				time += n.duration;
+				if (n.toString().length() > width)
+					width = n.toString().length();
+				if (n.chord) {
+					this.curtime = 0;
+					this.curpart++;
+				} else {
+					curpart = 0;
+				}
+				put(n, curpart, curtime);
+				curtime += n.duration;
 			}
 			//System.out.printf("note=%s, x=%.0f y=%.0f,\tval=%s\n", pitchname, x, y / 5 + 3, noteVal(pitchname));
 			//System.out.printf("%d\n");
 		}
 	}
+
+	void append(Note n, int aPart) {
+		
+	}
+	
 	int maxPart;
 	int maxTime;
 	void put(Note n, int aPart, int aTime) {
@@ -63,8 +93,10 @@ class Measure {
 			//			if (Notes[aPart] == null)
 			//				Notes[aPart] = new Note[subdiv * 4];
 			Notes[aPart][aTime] = n;
-			if(aPart > maxPart) maxPart = aPart;
-			if(aTime > maxTime) maxTime = aTime;
+			if (aPart > maxPart)
+				maxPart = aPart;
+			if (aTime > maxTime)
+				maxTime = aTime;
 			//			aTime += n.duration;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -72,10 +104,9 @@ class Measure {
 		}
 
 	}
+
 	void trim() {
-		int h = 0;
-		int w = 0;
-		Note[][] temp = new Note[maxPart+1][maxTime+1];
+		Note[][] temp = new Note[maxPart + 1][maxTime + 1];
 
 		for (int i = 0; i < temp.length; i++) {
 			for (int j = 0; j < temp[0].length; j++) {
@@ -83,36 +114,50 @@ class Measure {
 			}
 		}
 		Notes = temp;
-//		System.out.println("DONE");
+		//		System.out.println("DONE");
 	}
-	static String header(int len) {
+
+	static String header() {
 		StringBuilder sb = new StringBuilder();
-//		StringBuilder sb2 = new StringBuilder();
-		for (int i = 0; i < len; i++) {
-//			sb.append(i+1);
-			sb.append(String.format("b%-"+width+"d", i+1));
-//			sb2.append("+     ");
-		}
 		sb.append("\n");
-//		sb2.append("\n");
-//		sb.append(sb2.toString());
+		//		StringBuilder sb2 = new StringBuilder();
+		int len = notesperbeat*beatsperbar;
+		int tempw = width;
+		for (int i = 0; i < len; i++) {
+			//			sb.append(i+1);
+//			sb.append("|");
+			if(i%notesperbeat==0)
+				sb.append(String.format("b%-" + tempw + "d", i/notesperbeat+1));
+			else
+//				sb.append(".");
+				sb.append(String.format(".%-" + tempw + "s", ""));
+			//			sb2.append("+     ");
+			
+		}
+		sb.append("|\n");
+		//		sb2.append("\n");
+		//		sb.append(sb2.toString());
 		return sb.toString();
 	}
-	
-	
+	public String toString2() {
+		return String.format("Measure [sig=%d/%d, Notes=%s, curtime=%s, curpart=%s, measureNumber=%s, maxPart=%s, maxTime=%s]", beatsperbar,notesperbeat,Arrays.toString(Notes[0]), curtime, curpart, measureNumber,
+				maxPart, maxTime);
+	}
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(header(notesperbar));
+		sb.append(toString2());
+		sb.append(header());
 		for (int iPart = 0; iPart < Notes.length; iPart++) {
 			sb.append("|");
 			for (int iBeat = 0; iBeat < Notes[0].length; iBeat++) {
 				Note n = Notes[iPart][iBeat];
+//				sb.append("                    ".substring(0, width));
 
 				if (n == null)
-					sb.append("                    ".substring(0, width));
+					sb.append(String.format("%-" + width + "s", ""));
 				else
-					sb.append(String.format("%-"+width+"s", n));
+					sb.append(String.format("%-" + width + "s", n));
 				sb.append("|");
 			}
 			sb.append("\n");
